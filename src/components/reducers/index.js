eeaaa1f306738a8eaa5e6f454603779ebd934339
realisticattorney <g.axl.aquila@gmail.com>
2021-06-29 06:49:08 -0300

    Add index and reducers files

---
+++ src/components/reducers/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+import { combineReducers, applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+// import stocksRed from './reducer';
+// import selStockRed from './itemReducer';
+
+export const rootReducer = combineReducers();
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+export default store;
